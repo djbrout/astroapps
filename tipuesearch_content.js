d6251c5d01bfcbd0fe255468cce7a1cff9cd6546
Dillon Brout <djbrout@gmail.com>
2020-12-17 18:45:04 -0500

    reorganizing. making landing page

---
+++ tipuesearch_content.js
@@ -0,0 +1,1 @@
+var tipuesearch = {"pages":[{"title":"Einstein Fellowship (Accepted)","text":"This is the content. Should include a download link and preview. This browser does not support PDFs. Please download the PDF to view it: Download PDF .","tags":"Application","url":"einstein1.html","loc":"einstein1.html"}]};
